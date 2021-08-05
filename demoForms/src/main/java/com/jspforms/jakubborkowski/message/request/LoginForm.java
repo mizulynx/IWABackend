21f23726a9c9d35c3712a8ddde48703ce62826c1
mizuLynx <jakbor608@gmail.com>
2021-08-05 22:26:21 +0200

    kekjo

--- demoForms/src/main/java/com/jspforms/jakubborkowski/message/request/LoginForm.java
+++ demoForms/src/main/java/com/jspforms/jakubborkowski/message/request/LoginForm.java
@@ -12,7 +12,7 @@ public class LoginForm {
         private String username;
 
         @NotBlank
-        @Size(min = 6, max = 40)
+        @Size(min = 5, max = 40)
         private String password;
 
         public String getUsername() {
